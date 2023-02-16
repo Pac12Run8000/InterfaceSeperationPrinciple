a2bad0374b61f3581111524d0a763a07fea3c8bf
Pac12Run8000 <gangstarr78992@>
2023-02-16 12:54:35 -0800

    Initial Commit

---
+++ InterfaceSeperationPrinciple/InterfaceSeperationPrincipleApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterfaceSeperationPrincipleApp.swift
+//  InterfaceSeperationPrinciple
+//
+//  Created by Michelle Grover on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct InterfaceSeperationPrincipleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
